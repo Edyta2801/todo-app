7e4b4171d72a6d6a52ef4aef768a9d8a70185e01
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-27 12:04:40 +0100

    add key with data to datebase

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ const API_URL = 'https://monday-482dc.firebaseio.com';
 
 class App extends Component {
   state = {
-    tasks: [
-      { taskName: 'Odkurzanie', completed: false },
-      { taskName: 'Nakarmić kota', completed: false }
-    ],
+    tasks: [],
     taskName: ''
   }
 
@@ -22,24 +19,31 @@ class App extends Component {
     fetch(`${API_URL}/tasks.json`)
       .then(response => response.json())
       .then(data => {
-        const array=Object.entries(data);
-        cons=taskList=array.map(task=>task[1])
-        this.setState({tasks:tasksList})
-      })
+        const array = Object.entries(data);//index 0-klucze, index 1-obiekty zadań
+        console.log(data)
+        const tasksList = array.map(([id, values]) => {
+          values.id = id; //stworzenie nowej właściwości w obiekcie  zadania
+          return values;
+        });
+        this.setState({ tasks: tasksList, taskName: '' });
+      });
   }
 
 
-  handleClick = (event) => {
+  handleClick = () => {
     if (this.state.taskName !== '') {
       let tasks = this.state.tasks;
       const newTask = { taskName: this.state.taskName, completed: false };
       fetch(`${API_URL}/tasks.json`, {
         method: 'POST',
         body: JSON.stringify(newTask)
-      }).then(() => {
-        tasks.push(newTask);
-        this.setState({ tasks, taskName: '' })
       })
+        .then(response => response.json())
+        .then(data => {
+          newTask.id = data.name;
+          tasks.push(newTask);
+          this.setState({ tasks, taskName: '' })
+        });
     }
   }
 
@@ -55,8 +59,8 @@ class App extends Component {
           <FlatButton label="Add" primary={true} onClick={this.handleClick} />
         </div>
         <div>
-          {this.state.tasks.map((task, index) => (
-            <div>{task.taskName}</div>
+          {this.state.tasks.map((task) => (
+            <div key={task.id}>{task.taskName}</div>
           ))}
         </div>
       </div>
