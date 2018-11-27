67c0dea9fe3dcf4d227edb14b31fb77b34fc3a73
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-27 15:02:59 +0100

    Updates

---
+++ precache-manifest.2bc335068aa782f25bfff4eaebc2e5a0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "c05173387be047d49248",
+    "url": "/todo-app/static/js/main.c0517338.chunk.js"
+  },
+  {
+    "revision": "c63261a0ac353ee4e2ca",
+    "url": "/todo-app/static/js/1.c63261a0.chunk.js"
+  },
+  {
+    "revision": "6f097e70b7ce7ab40a0c8e77f1da04f8",
+    "url": "/todo-app/index.html"
+  }
+];
