d8e795abe9c2cebc1058ce2bda3b42109dba5505
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-27 11:35:59 +0100

    finished componentWillMount

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TextField from 'material-ui/TextField';
 import FlatButton from 'material-ui/FlatButton';
 
 
-const API_URL='https://monday-482dc.firebaseio.com';
+const API_URL = 'https://monday-482dc.firebaseio.com';
 
 class App extends Component {
   state = {
@@ -17,39 +17,51 @@ class App extends Component {
   handleChange = (event) => {
     this.setState({ taskName: event.target.value });
   }
-  handleClick = () => {
+
+  componentWillMount() {
+    fetch(`${API_URL}/tasks.json`)
+      .then(response => response.json())
+      .then(data => {
+        const array=Object.entries(data);
+        cons=taskList=array.map(task=>task[1])
+        this.setState({tasks:tasksList})
+      })
+  }
+
+
+  handleClick = (event) => {
     if (this.state.taskName !== '') {
       let tasks = this.state.tasks;
-      const newTask={taskName:this.state.taskName, completed:false};
-      fetch('https://monday-482dc.firebaseio.com/tasks.json'), {
+      const newTask = { taskName: this.state.taskName, completed: false };
+      fetch(`${API_URL}/tasks.json`, {
         method: 'POST',
         body: JSON.stringify(newTask)
-      }).then(()=>{
+      }).then(() => {
         tasks.push(newTask);
-        this.setState({ tasks, taskName:'' });
+        this.setState({ tasks, taskName: '' })
       })
     }
   }
 
 
 
-render() {
-  return (
-    <div className="App">
-      <div>
-        <TextField hintText="Enter you task here"
-          value={this.state.taskName}
-          onChange={this.handleChange} />
-        <FlatButton label="Add" primary={true} onClick={this.handleClick} />
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <TextField hintText="Enter you task here"
+            value={this.state.taskName}
+            onChange={this.handleChange} />
+          <FlatButton label="Add" primary={true} onClick={this.handleClick} />
+        </div>
+        <div>
+          {this.state.tasks.map((task, index) => (
+            <div>{task.taskName}</div>
+          ))}
+        </div>
       </div>
-      <div>
-        {this.state.tasks.map((task, index) => (
-          <div>{task.taskName}</div>
-        ))}
-      </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
