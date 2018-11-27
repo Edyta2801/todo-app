2a85295f357a92b77fb0f393f968c6306e7f0296
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-11-27 10:41:18 +0100

    add set.state an material-ui TextField and FlatButton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 
 
 class App extends Component {
+  state = {
+    tasks: [
+      { taskName: 'Odkurzanie', completed: false },
+      { taskName: 'Nakarmić kota', completed: false }
+    ],
+    taskName: ''
+  }
+
+
   render() {
     return (
-      <div className="App" >
+      <div className="App">
+        <div>
+          <TextField hintText="Enter you task here" />
+          <FlatButton label="Add" primary={true} />
+        </div>
+        <div>
+          {this.state.tasks.map((task, index) => (
+            <div>{task.taskName}</div>
+          ))}
+        </div>
       </div>
     );
   }
